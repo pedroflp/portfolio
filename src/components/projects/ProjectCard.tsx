dea69fff5e38fc638c2df2df4c7bffb7d9fc7f9f
Pedroflp <pedroflp@ufrn.edu.br>
2021-02-05 16:41:40 -0300

    Atualizando ProjectCard Component

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -35,13 +35,13 @@ function ProjectCard(props: ProjectCard) {
 
     <div className='projects-card-buttons'>
       <span>Visualizar projeto:</span>
-      { props.ghLink != "" && <a className='github projects-card-button' href={props.ghLink} target='_blank' >
+      { props.ghLink && <a className='github projects-card-button' href={props.ghLink} target='_blank' >
         <AiFillGithub size={30} />
       </a> }
 
-      { props.brLink != "" && <a className='browser projects-card-button' href={props.brLink} target='_blank' >
+      { props.brLink && <a className='browser projects-card-button' href={props.brLink} target='_blank' >
         <RiChromeFill size={30} />
-      </a>}
+      </a> }
     </div>
 
       </>
